66c78e621a0801e19587262cf8b3a8ee90f0d8ac
luizlee107 <luiz014@hotmail.com>
2024-01-22 02:29:45 -0300

    add-style

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
 
     #verify 2 functions in the same page
-    path('product', views.product),
+    path('product', views.product, name='product'),
     path('product_list', views.product_list, name='product_list'),
     path('product/add', views.add_product, name='add_product'),
     path('products/<int:pk>/remove', views.remove_product, name='remove_product'),
